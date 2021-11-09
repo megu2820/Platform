76e91bf1e95218a42aadc9f173c8208a417d4764
megu2820 <meghasharma2820@gmail.com>
2021-11-09 15:44:28 +0530

    Long time huh.. :sparkles:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import 'tailwindcss/tailwind.css'
+import React ,{useEffect,useState} from 'react'
+import { Layout } from '../Components';
+
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+  <Layout>
+ <Component {...pageProps} />
+
+  </Layout>
+ )
 }
 
 export default MyApp
