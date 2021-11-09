36fe36352e91f0d1f2cbde50566920741b0aa2d9
megu2820 <66476812+megu2820@users.noreply.github.com>
2021-11-09 16:38:13 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       'bg-blue-500',
       'text-center',
       'hover:opacity-100',
+      'text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20',
 
       'lg:text-right',
     ]
