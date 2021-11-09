c535f4d4526abb7fef1111886a2b018e71e9858e
megu2820 <66476812+megu2820@users.noreply.github.com>
2021-11-09 16:48:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,4 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    safelist: [
-      'bg-blue-500',
-      'text-center',
-      'hover:opacity-100',
-      'text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20',
 
-      'lg:text-right',
-    ]
-  },
-  variants: {
-    extend: {},
-  },
   plugins: [],
 }
